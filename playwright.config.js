b3804c13ac5095e3f1351f9d7b9e841f58f3cf86
muthukmr621@gmail.com <muthukmr621@gmail.com>
2024-11-06 14:52:53 +0530

    modified config files

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,6 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
   testDir: './tests',
-  workers: 5,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -20,7 +19,7 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  //workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -32,7 +31,7 @@ module.exports = defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'on',
-    headless: false,  // Enable headed mode by setting this to false
+    headless: true,  // Enable headed mode by setting this to false
 
   },
 
